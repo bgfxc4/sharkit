064c73a71dd62431e53bcb670a7ae107b43033ec
bgfxc4 <bindermarc@gmx.de>
2021-06-27 23:39:31 +0200

    added basic text input for insert mode

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ const MODES = {
 }
 
 var mode = MODES.NORMAL
+var is_first_press_after_mode_switch = false;
 
 var text_storage = [
 	"1. line",
@@ -46,26 +47,98 @@ function render_text() {
 function render_cursor() {
 	if (mode == MODES.NORMAL) {
 		fill(255)
-		rect(textWidth(text_storage[cursor_pos.y].substring(0, cursor_pos.x)) + text_size, cursor_pos.y * text_size, textWidth(text_storage[cursor_pos.y][cursor_pos.x]), text_size * 1.15)
+		rect(textWidth(text_storage[cursor_pos.y].substring(0, cursor_pos.x)) + text_size, 
+			cursor_pos.y * text_size + 0.2 * text_size,
+			textWidth(text_storage[cursor_pos.y][cursor_pos.x]), text_size)
 		fill(0)
 		text(text_storage[cursor_pos.y][cursor_pos.x], textWidth(text_storage[cursor_pos.y].substring(0, cursor_pos.x)) + text_size, cursor_pos.y * text_size + text_size)
+	} else if (mode == MODES.INSERT) {
+		fill(255)
+		rect(textWidth(text_storage[cursor_pos.y].substring(0, cursor_pos.x)) + text_size, 
+			cursor_pos.y * text_size + 0.2 * text_size,
+			2, text_size)
 	}
 }
 
+function switch_mode(mode_to_switch_to) {
+	mode = mode_to_switch_to
+	is_first_press_after_mode_switch = true
+}
 
 function keyPressed() {
 	if (mode == MODES.NORMAL) {
-		if (keyIsDown(LEFT_ARROW)) { // move cursor
+		if (keyCode == LEFT_ARROW) { // move cursor
 			if (cursor_pos.x > 0) cursor_pos.x--
-		} else if (keyIsDown(RIGHT_ARROW)) {
+		} else if (keyCode == RIGHT_ARROW) {
 			if (text_storage[cursor_pos.y][cursor_pos.x + 1] != undefined) cursor_pos.x++
-		} else if (keyIsDown(DOWN_ARROW)) {
+		} else if (keyCode == DOWN_ARROW) {
 			if (text_storage[cursor_pos.y + 1] != undefined) {
 				if (text_storage[cursor_pos.y + 1][cursor_pos.x] == undefined) cursor_pos.x = text_storage[cursor_pos.y + 1].length - 1
 				cursor_pos.y++
 			}
-		} else if (keyIsDown(UP_ARROW)) {
-			if (cursor_pos.y > 0) cursor_pos.y--
+		} else if (keyCode == UP_ARROW) {
+			if (cursor_pos.y > 0) {
+				if (text_storage[cursor_pos.y - 1][cursor_pos.x] == undefined) cursor_pos.x = text_storage[cursor_pos.y - 1].length - 1
+				cursor_pos.y--
+			}
 		}
+
+		else if (key === 'i') { // set modes
+			switch_mode(MODES.INSERT)
+		}
+	} else if (mode == MODES.INSERT) {
+		if (keyCode == ESCAPE) { // change mode back to normal
+			switch_mode(MODES.NORMAL)
+			if (text_storage[cursor_pos.y][cursor_pos.x] == undefined) cursor_pos.x = text_storage[cursor_pos.y].length - 1
+		}
+
+		if (keyCode == LEFT_ARROW) { // move cursor
+			if (cursor_pos.x > 0) cursor_pos.x--
+		} else if (keyCode == RIGHT_ARROW) {
+			if (text_storage[cursor_pos.y][cursor_pos.x] != undefined) cursor_pos.x++
+		} else if (keyCode == DOWN_ARROW) {
+			if (text_storage[cursor_pos.y + 1] != undefined) {
+				if (text_storage[cursor_pos.y + 1][cursor_pos.x] == undefined) cursor_pos.x = text_storage[cursor_pos.y + 1].length
+				cursor_pos.y++
+			}
+		} else if (keyCode == UP_ARROW) {
+			if (cursor_pos.y > 0) {
+				if (text_storage[cursor_pos.y - 1][cursor_pos.x] == undefined) cursor_pos.x = text_storage[cursor_pos.y - 1].length
+				cursor_pos.y--
+			}
+		}
+
+		else if (keyCode == BACKSPACE) { // delete char
+			if (cursor_pos.x > 0) {
+				text_storage[cursor_pos.y] = text_storage[cursor_pos.y].slice(0, cursor_pos.x - 1) + 
+					text_storage[cursor_pos.y].slice(cursor_pos.x, text_storage[cursor_pos.y].length)
+				cursor_pos.x--
+			} else if (cursor_pos.y > 0) {
+				cursor_pos.x = text_storage[cursor_pos.y - 1].length // remove line break
+				text_storage[cursor_pos.y - 1] += text_storage[cursor_pos.y]
+				text_storage.splice(cursor_pos.y, 1)
+				cursor_pos.y--
+			}
+		} else if (keyCode == ENTER) { // new line break
+			text_storage.splice(cursor_pos.y + 1, 0, text_storage[cursor_pos.y].substring(cursor_pos.x))
+			text_storage[cursor_pos.y] = text_storage[cursor_pos.y].substring(0, cursor_pos.x)
+			cursor_pos.y++
+			cursor_pos.x = 0
+		}
+	}
+}
+
+function keyTyped() { // using different function for text input, bc it does not react to special keys
+	if (is_first_press_after_mode_switch) {
+		is_first_press_after_mode_switch = false
+		return
+	}
+	
+	if (key == "Enter") return
+
+	if (mode == MODES.INSERT) {
+		text_storage[cursor_pos.y] = text_storage[cursor_pos.y].slice(0, cursor_pos.x) + key 
+			+ text_storage[cursor_pos.y].slice(cursor_pos.x)
+		cursor_pos.x++
 	}
 }
