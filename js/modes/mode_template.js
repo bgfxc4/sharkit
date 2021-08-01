13d381ffdff98780f1b4956a25c48832a7e7b432
bgfxc4 <bindermarc@gmx.de>
2021-08-01 02:05:41 +0200

    added mode template

---
+++ js/modes/mode_template.js
@@ -0,0 +1,11 @@
+var COMMAND_MODE = {
+	modeText: "",
+	
+	keyPressed: function() {},
+
+	keyTyped: function() {},
+
+	mousePressed: function(clicked_x, clicked_y) {},
+
+	render_cursor: function () {},
+}
